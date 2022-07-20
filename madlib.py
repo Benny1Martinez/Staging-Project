b1dc3395869663f507bf2518c5c47b8fffefb81e
Benny <benny.martinez@gmail.com>
2022-07-20 13:12:03 -0700

    commin

--- madlib.py
+++ madlib.py
@@ -1,10 +1,10 @@
 adj1 = input("Adjective: ")
 verb1 = input("Verb ending in -ed: ")
+activity = input("Activity: ")
 verb2 = input("Verb: ")
-verb3 = input("Verb: ")
 famous_person = input("Famous person: ")
 
-madlib = f"Computer Programming is {adj1}! It makes me so {verb1} all the time beacuse\
-    I love to {verb2}. Stay hydrated and {verb3} as if you were {famous_person}!"
+madlib = f"Computer Programming is {adj1}! It makes me so {verb1} all the time beacuse \
+I love to {activity}. Stay hydrated and {verb2} as if you were {famous_person}!"
 
 print(madlib)
